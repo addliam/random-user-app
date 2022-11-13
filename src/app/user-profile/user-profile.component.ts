3d8329528951dfc4b6c1a446ffae428d52e3b527
addliam <liamyostin2004@gmail.com>
2022-11-13 13:01:04 -0500

    add user profile information: personal, location n contact

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,14 +8,14 @@ import { ApiService } from '../api.service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  userData:any;
+  userData?:User;
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getRandomUser()
-      .subscribe((data) => {
-        console.log(data)
-        this.userData = data
+      .subscribe((data: any) => {
+        console.log(data.results[0] as User)
+        this.userData = data.results[0];
       });
   }
 
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/app/**/*.{html,ts}",    
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blackDark: "#1D242E",
+        blackPrimary: "#283342",
+        blackContrast: "#009099",
+        white: "#FFFFFF",
+        black: "#1D242E",
+        blackOverlay: "rgba(29, 36, 46, 0.5)",
+        yellow: "#FED600",
+        orange: "#F0483E",
+        orangeContrast: "#E0443B",
+        red: "#F0483E",
+        skyBlue: "#03A9F5",
+        green: "#01A768",
+        whiteGray: "#EDF1F5"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
